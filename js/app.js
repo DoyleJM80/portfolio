fb72b7ae1b89d99aff4babd1b659cc1af9ec2b1c
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-08-05 14:17:58 -0400

    added styling

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+  var contactDisplay = document.getElementById('contact-display');
+  var phone = document.getElementById('phone-icon');
+  var email = document.getElementById('email-icon');
+  var phoneNumber = '864-322-3201';
+  var address = 'thedoyles922@gmail.com'
+  console.log(contactDisplay);
+  console.log(phone);
+  console.log(email);
+
+  phone.onclick = function() {
+    contactDisplay.innerHTML = phoneNumber;
+  }
+
+  email.onclick = function() {
+    contactDisplay.innerHTML = address;
+  }
